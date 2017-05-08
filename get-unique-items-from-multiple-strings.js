f423463247d440718cda76362ed02f20f209398e
Josef Zacek <info@josefzacek.com>
2017-05-08 10:46:04 +0100

    get elements, replace unwanted items, split them, trim empty space and push elements to arrayOfItems

--- get-unique-items-from-multiple-strings.js
+++ get-unique-items-from-multiple-strings.js
@@ -1,1 +1,11 @@
 // http://bpweb.searchoptics.com/movidaseminovos.com.br/seminovos/estoque-opcionais.html
+
+var arrayOfItems = [];                                                                                                                  // declare arrayOfItems array variable
+$("#panel-custom_field_3 .option .text-wrap").each(function(){                                                                          // loop through items
+  string = $(this).text().replace(/\(.*?\)/, "").replace("n/a", "").replace("N / a", "").replace("Mais...", "").replace("n / a","");    // replace things such as (50), n/a, N / a, Mais..., n / a
+  fullArray = string.split(',');                                                                                                        // split items by comma and store it in fullArray variable
+  $.each(fullArray, function(index, value) {                                                                                            // loop through fullArray variable
+    element = $.trim(value);                                                                                                            // remove empty sdtapecs before and after element
+    arrayOfItems.push(element);                                                                                                         // push element to arrayOfItems array
+  });
+});
